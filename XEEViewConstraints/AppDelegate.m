d23b2aa391620ce3e9acbc36e4c6eb54fd6fa295
Andrija Cajic <ancajic@gmail.com>
2013-11-14 16:01:14 +0100

    no message

--- XEEViewConstraints/AppDelegate.m
+++ XEEViewConstraints/AppDelegate.m
@@ -14,11 +14,8 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    
     self.window.rootViewController = [[ViewController alloc] init];
-    
     [self.window makeKeyAndVisible];
-    
     return YES;
 }
 							
